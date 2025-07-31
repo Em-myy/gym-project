648abbb51caac641af619f06360ae9b28b5db80a
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2025-07-31 12:11:43 +0100

    Image settings for hero

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -3,7 +3,10 @@ const Hero = () => {
   return (
     <div className="h-screen">
       <div className="flex flex-col justify-center items-center h-1/2 mt-10 gap-y-4 md:pb-[50px]">
-        <img className="w-[200px] h-[200px]" src={image1} />
+        <img
+          className=" w-[50px] h-[50px] md:w-[200px] md:h-[200px]"
+          src={image1}
+        />
         <p className="text-8xl">Classes</p>
         <p className="text-4xl">Home / Classes</p>
       </div>
