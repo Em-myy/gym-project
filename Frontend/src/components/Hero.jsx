79e05b467f2e991bf3a5590912645dccf1f7f39c
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2025-07-31 12:16:50 +0100

    fixed error and removed unused imports

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,12 +1,7 @@
-import image1 from "/gym_7924091.png";
 const Hero = () => {
   return (
     <div className="h-screen">
-      <div className="flex flex-col justify-center items-center h-1/2 mt-10 gap-y-4 md:pb-[50px]">
-        <img
-          className=" w-[50px] h-[50px] md:w-[200px] md:h-[200px]"
-          src={image1}
-        />
+      <div className="flex flex-col justify-center items-center h-1/2 gap-y-4 md:pb-[50px]">
         <p className="text-8xl">Classes</p>
         <p className="text-4xl">Home / Classes</p>
       </div>
