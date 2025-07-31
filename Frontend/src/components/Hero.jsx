36c1f701215962ca4d8a0b009c3dda2662b82334
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2025-07-31 12:06:26 +0100

    Image added to hero component

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,7 +1,9 @@
+import image1 from "/gym_7924091.png";
 const Hero = () => {
   return (
     <div className="h-screen">
-      <div className="flex flex-col justify-center items-center h-1/2 gap-y-4 md:pb-[50px]">
+      <div className="flex flex-col justify-center items-center h-1/2 mt-10 gap-y-4 md:pb-[50px]">
+        <img className="w-[200px] h-[200px]" src={image1} />
         <p className="text-8xl">Classes</p>
         <p className="text-4xl">Home / Classes</p>
       </div>
